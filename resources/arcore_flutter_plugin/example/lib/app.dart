bb8b1bc39c0461f7d7fdd37a871afdaf53c37288
Peter Merikan <peter@merikan.com>
2021-11-12 10:12:02 +0100

    add resource

---
+++ resources/arcore_flutter_plugin/example/lib/app.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //where to go
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World - ArCore Pluggin"),
+        ),
+      ),
+    );
+  }
+}
